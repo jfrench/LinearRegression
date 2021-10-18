986999ad69bc0c1d79e222fc7ba776f2eeb7192c
French <JOSHUA.FRENCH@UCDENVER.EDU>
2021-10-18 12:34:12 -0600

    Update orthogonal section.

--- code/06b-Diabetes-Example.R
+++ code/06b-Diabetes-Example.R
@@ -16,16 +16,16 @@ fitted(lmod)
 fitted(lmod)[3]
 lmod$fitted.values[3]
 
-yhat <- fitted(lmod)
-
 # residual plot
 plot(residuals(lmod) ~ fitted(lmod))
 abline(0, 0, lty = 2)
 
 # y vs yhat
+# do this for y because the original data have NA values
+# lmod$model$chol is the y values for the rows without missing data
 plot(lmod$model$chol ~ fitted(lmod), xlab = "yhat", ylab = "y")
 abline(lm(lmod$model$chol ~ fitted(lmod)))
-summary(lmod)$r.squared
+summary(lmod)$r.squared # r-squared
 
 # orthogonality
 cov(model.matrix(lmod))
